c042825bdf0194fe3cf6a877054216befc6abcb5
zhouhao <zhouzhou1992@live.com>
2018-07-27 16:31:11 +0800

    打开eureka

--- src/main/java/com/netflix/hystrix/dashboard/HystrixDashboardApplication.java
+++ src/main/java/com/netflix/hystrix/dashboard/HystrixDashboardApplication.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static com.netflix.hystrix.dashboard.data.netty.NettyStarter.HYSTRIX_STREAM_LOCAL_ADDRESS;
@@ -16,8 +15,7 @@ import static com.netflix.hystrix.dashboard.data.netty.NettyStarter.HYSTRIX_STRE
 
 @SpringBootApplication(scanBasePackages = { "com" })
 @EnableScheduling
-//@EnableDiscoveryClient
-//@EnableFeignClients
+@EnableDiscoveryClient
 public class HystrixDashboardApplication{
 
     public static void main(String[] args) {
