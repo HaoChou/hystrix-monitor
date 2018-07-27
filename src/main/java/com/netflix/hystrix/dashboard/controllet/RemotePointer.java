b21edb4ecd1d5144a37ec56f98688b758e5ac97b
zhouhao <zhouzhou1992@live.com>
2018-07-27 11:07:40 +0800

    test

--- src/main/java/com/netflix/hystrix/dashboard/controllet/RemotePointer.java
+++ src/main/java/com/netflix/hystrix/dashboard/controllet/RemotePointer.java
@@ -1,5 +1,6 @@
 package com.netflix.hystrix.dashboard.controllet;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,14 +8,12 @@ import java.util.Map;
  * @author zhou
  * Created on 2018/7/26
  */
-public class RemotePointer {
+public class RemotePointer implements Serializable {
 
     private  String measurement;
     private  Map<String,String> tagMaps;
     private  Map<String,Number> fieldMaps;
 
-    public RemotePointer() {
-    }
 
     public RemotePointer(String measurement){
         this.measurement=measurement;
